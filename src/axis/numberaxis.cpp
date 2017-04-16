dd68e6e2cf1e1aafbb9a057b4bafa9d401f0c7e6
iwbnwif <iwbnwif@gmail.com>
2017-04-16 21:33:18 +0100

    Tidy up comment formatting.

--- src/axis/numberaxis.cpp
+++ src/axis/numberaxis.cpp
@@ -1,10 +1,10 @@
 /////////////////////////////////////////////////////////////////////////////
-// Name:    numberaxis.cpp
-// Purpose: number axis implementation
-// Author:    Moskvichev Andrey V.
-// Created:    2008/11/07
-// Copyright:    (c) 2008-2010 Moskvichev Andrey V.
-// Licence:    wxWidgets licence
+// Name:        numberaxis.cpp
+// Purpose:     number axis implementation
+// Author:      Moskvichev Andrey V.
+// Created:     2008/11/07
+// Copyright:   (c) 2008-2010 Moskvichev Andrey V.
+// Licence:     wxWidgets licence
 /////////////////////////////////////////////////////////////////////////////
 
 #include <wx/axis/numberaxis.h>
