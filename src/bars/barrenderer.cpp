34e7b315b236bb63ce5a0c91fcc36007a33e0006
iwbnwif <iwbnwif@gmail.com>
2021-08-22 17:07:49 +0100

    Improve stacked bars to allow more than two data series.
    * Fix for adding 3 or more series as stacked bars
    
    * Adjusted demo to show 3 stacked bars
    
    Improvements by QFer.

--- src/bars/barrenderer.cpp
+++ src/bars/barrenderer.cpp
@@ -161,7 +161,7 @@ void StackedBarType::GetBarGeometry(CategoryDataset *dataset, size_t item, size_
 {
     width = m_barWidth;
     shift = -m_barWidth / 2;
-    base = (serie >= 1) ? dataset->GetValue(item, serie - 1) : m_base;
+    base = (serie >= 1) ? base + dataset->GetValue(item, serie - 1) : m_base;
     value = dataset->GetValue(item, serie);
     if (serie >= 1) {
         value += base;
